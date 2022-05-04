a575116d89a66bba4da47d23d1e2932bbbdccee1
vitalekp <vitalekpushkarov@gmail.com>
2022-05-04 13:24:36 +0300

    solution TODO list

--- js/events/solution.js
+++ js/events/solution.js
@@ -40,31 +40,31 @@ const tasks = [
 
 // LIZA - good
 
-// const taskStatusHandler = event => {
-//   const taskId = Number(event.target.dataset.id);
-//   const currentTask = tasks.find(task => task.id === taskId);
-//   currentTask.done = !currentTask.done;
-//   renderTasks(tasks);
-// };
+const taskStatusHandler = event => {
+  const taskId = Number(event.target.dataset.id);
+  const currentTask = tasks.find(task => task.id === taskId);
+  currentTask.done = !currentTask.done;
+  renderTasks(tasks);
+};
 
-// listElem.addEventListener('click', taskStatusHandler);
+listElem.addEventListener('click', taskStatusHandler);
 
-// const createTaskHandler = () => {
-//   const inputElem = document.querySelector('.task-input');
-//   const newInput = inputElem.value;
+const createTaskHandler = () => {
+  const inputElem = document.querySelector('.task-input');
+  const newInput = inputElem.value;
 
-//   if (!newInput) {
-//     return;
-//   }
+  if (!newInput) {
+    return;
+  }
 
-//   tasks.push({
-//     text: newInput,
-//     done: false,
-//     id: tasks.length + 1,
-//   });
+  tasks.push({
+    text: newInput,
+    done: false,
+    id: tasks.length + 1,
+  });
 
-//   inputElem.value = '';
-//   renderTasks(tasks);
-// };
+  inputElem.value = '';
+  renderTasks(tasks);
+};
 
-// buttonELem.addEventListener('click', createTaskHandler);
+buttonELem.addEventListener('click', createTaskHandler);
