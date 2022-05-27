5042baa12bf964cea1f5669bac4314656de7398c
vitalekp <vitalekpushkarov@gmail.com>
2022-05-27 17:23:55 +0300

    get url params

--- js/get-url-example/index.js
+++ js/get-url-example/index.js
@@ -1,14 +1,19 @@
-const locationUrl = 'orderField=id&page=22&orderMode=desc&status=1';
+const locationUrl = 'orderField=id&page=32&orderMode=desc&status=1';
 
 // разделить по амперсанту
 // найти елемент с строкой page
 // разделить по '='
 // взять второй eлемент
 
-const getPage = url =>
-  url
-    .split('&')
-    .find(el => el.includes('page'))
-    .split('=')[1];
+const searchParams = new URLSearchParams(locationUrl);
+console.log(searchParams.get('page'));
 
-console.log(locationUrl.slice(locationUrl.indexOf('page'), locationUrl.indexOf('&o')));
+// const getPage = url =>
+//   url
+//     .split('&')
+//     .find(el => el.includes('page'))
+//     .split('=')[1];
+
+// console.log(getPage(locationUrl));
+
+// console.log(locationUrl.slice(locationUrl.indexOf('page'), locationUrl.indexOf('&o')));
