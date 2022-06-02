46a39f6ed629a11c21d20fb495fad4ec55f7410a
vitalekp <vitalekpushkarov@gmail.com>
2022-06-02 11:30:34 +0300

    spinner improve

--- react/layout/src/Spinner.jsx
+++ react/layout/src/Spinner.jsx
@@ -4,7 +4,11 @@ import React from 'react';
 // out: jsx
 const Spinner = props => {
   return (
-    <div className="lds-ring">
+    <div className="lds-roller">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
       <div></div>
       <div></div>
       <div></div>
