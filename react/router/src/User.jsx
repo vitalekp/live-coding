cad9c690cb3bd2f2204635437b6d88401846d653
vitalekp <vitalekpushkarov@gmail.com>
2022-07-05 19:37:10 +0300

    hooks

--- react/router/src/User.jsx
+++ react/router/src/User.jsx
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-const User = ({ match }) => {
-  console.log(match.params.userId);
+const User = props => {
+  // input: any
+  // out: array [any, func] - length 2
+  const [userInfo, setUserInfo] = useState({
+    avatarUrl: null,
+    location: null,
+    name: null,
+  });
+  const { userId } = useParams();
 
+  // input: func, arr
+  // output: undef
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${userId}`)
+      .then(response => response.json())
+      .then(res =>
+        setUserInfo({
+          avatarUrl: res.avatar_url,
+          location: res.location,
+          name: res.name,
+        }),
+      );
+  }, [userId]);
+
+  const { avatarUrl, location, name } = userInfo;
+
+  if (avatarUrl === null) {
+    return null;
+  }
   return (
     <div className="user">
-      <img
-        alt="User Avatar"
-        src="https://avatars1.githubusercontent.com/u/9919?v=4"
-        className="user__avatar"
-      />
+      <img alt="User Avatar" src={avatarUrl} className="user__avatar" />
       <div className="user__info">
-        <span className="user__name">GitHub</span>
-        <span className="user__location">San Francisco,CA</span>
+        <span className="user__name">{name}</span>
+        <span className="user__location">{location}</span>
       </div>
     </div>
   );
